da704254c78eb7db053ec66066b35fe0fd4f0ff3
David MENTRE <dmentre@linux-france.org>
2013-12-18 20:04:15 +0100

    [condorcet] Rename Reset_To_Zero to Reset. Add Post to Reset

--- condorcet/src/condorcet_matrix.adb
+++ condorcet/src/condorcet_matrix.adb
@@ -1,7 +1,11 @@
 package body Condorcet_Matrix is
-   procedure Reset_To_Zero(M : in out Condorcet_Matrix) is
+   function Is_Zero(M : Condorcet_Matrix) return Boolean is
+     (for all I in Candidate_Range'Range =>
+        (for all J in Candidate_Range'Range => M.Vote(I,J) = 0));
+
+   procedure Reset(M : in out Condorcet_Matrix) is
    begin
       M.Size := Candidate_Range'First;
       M.Vote := (others => (others => 0));
-   end Reset_To_Zero;
+   end Reset;
 end Condorcet_Matrix;
--- condorcet/src/condorcet_matrix.ads
+++ condorcet/src/condorcet_matrix.ads
@@ -1,11 +1,14 @@
 package Condorcet_Matrix is
-   type Candidate_Range is range 2..20; -- REQ-D-1
+   type Candidate_Range is range 1..20; -- REQ-D-1 for upper bound
 
    type Vote_Range is range 0..10_000; -- REQ-D-2 for upper bound
 
    type Condorcet_Matrix is private;
 
-   procedure Reset_To_Zero(M : in out Condorcet_Matrix);
+   function Is_Zero(M : Condorcet_Matrix) return Boolean;
+
+   procedure Reset(M : in out Condorcet_Matrix) with
+     Post => Is_Zero(M);
 
 private
    type Matrix_Content is
@@ -16,4 +19,5 @@ private
          Size : Candidate_Range;
          Vote : Matrix_Content;
       end record;
+
 end Condorcet_Matrix;
