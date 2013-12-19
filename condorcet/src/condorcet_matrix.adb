f994b1cf7eb9a1f7e8d93bfb868802bb4170356e
David MENTRE <dmentre@linux-france.org>
2013-12-19 06:09:13 +0100

    [condorcet] New functions and Is_Valid_Vote_Matrix. New procedure Vote
    * Update Reset to initialize the Size of M to a given size.

--- condorcet/src/condorcet_matrix.adb
+++ condorcet/src/condorcet_matrix.adb
@@ -1,11 +1,48 @@
 package body Condorcet_Matrix is
+   function Get_Size(M : Condorcet_Matrix) return Candidate_Range is
+     (M.Size);
+
+   function Is_Valid_Vote_Matrix(M : Condorcet_Matrix; Vote : Vote_T)
+                                 return Boolean is
+     (-- all votes of 'Vote' a properly stored
+        (for all I in Vote'Range =>
+           (for all J in I+1 .. Vote'Last =>
+              (M.Vote(Vote(I),Vote(J)) = 1 -- candidate I winner over J
+               and M.Vote(Vote(J),Vote(I)) = 0 -- and J loser over I
+              )))
+      and
+        -- stored votes are only coming from 'Vote'
+        (for all I in Candidate_Range'Range =>
+           (for all J in Candidate_Range'Range =>
+              (if M.Vote(I,J) = 1 then
+                 (for Some Ip in Vote'Range =>
+                    (for Some Jp in Vote'Range =>
+                       (Vote(Ip) = I and Vote(Jp) = J and Ip < Jp)
+                    ))
+               else M.Vote(I,J) = 0
+              )))
+        and
+        -- only zeros on the diagonal
+        (for all I in Candidate_Range'Range => M.Vote(I,I) = 0));
+
    function Is_Zero(M : Condorcet_Matrix) return Boolean is
      (for all I in Candidate_Range'Range =>
         (for all J in Candidate_Range'Range => M.Vote(I,J) = 0));
 
-   procedure Reset(M : in out Condorcet_Matrix) is
+   procedure Reset(M : in out Condorcet_Matrix; Size : Candidate_Range) is
    begin
-      M.Size := Candidate_Range'First;
+      M.Size := Size;
       M.Vote := (others => (others => 0));
    end Reset;
+
+   procedure Vote(M : in out Condorcet_Matrix; Vote : Vote_T) is
+   begin
+      for I in Vote'Range loop
+         for J in I+1 .. Vote'Last loop
+            M.Vote(Vote(I), Vote(J)) := 1;
+            M.Vote(Vote(J), Vote(I)) := 0;
+         end loop;
+      end loop;
+   end Vote;
+
 end Condorcet_Matrix;
--- condorcet/src/condorcet_matrix.ads
+++ condorcet/src/condorcet_matrix.ads
@@ -3,12 +3,39 @@ package Condorcet_Matrix is
 
    type Vote_Range is range 0..10_000; -- REQ-D-2 for upper bound
 
+   type Vote_T is array (Candidate_Range range <>) of Candidate_Range;
+
    type Condorcet_Matrix is private;
+
+   function Get_Size(M : Condorcet_Matrix) return Candidate_Range;
+
+   function Is_Valid_Vote(Vote : Vote_T; Size : Candidate_Range) return Boolean is
+     (-- 'Vote' range is valid
+      Vote'First = Candidate_Range'First and Vote'Last = Size
+      and
+      -- not twice the same candidate
+        (for all I in Vote'Range =>
+           (for all J in Vote'Range =>
+              (if I /= J then Vote(I) /= Vote(J))
+           ))
+      and
+        -- only vote for allowed candidates
+        (for all I in Vote'Range =>
+           (Vote(I) in Vote'Range))
+     );
+
+   function Is_Valid_Vote_Matrix(M : Condorcet_Matrix; Vote : Vote_T)
+                                 return Boolean with
+     Pre => Is_Valid_Vote(Vote, Get_Size(M));
 
    function Is_Zero(M : Condorcet_Matrix) return Boolean;
 
-   procedure Reset(M : in out Condorcet_Matrix) with
-     Post => Is_Zero(M);
+   procedure Reset(M : in out Condorcet_Matrix; Size : Candidate_Range) with
+     Post => Is_Zero(M) and Get_Size(M) = Size;
+
+   procedure Vote(M : in out Condorcet_Matrix; Vote : Vote_T) with
+     Pre => Is_Zero(M) and Is_Valid_Vote(Vote, Get_Size(M)),
+     Post => Is_Valid_Vote_Matrix(M, Vote);
 
 private
    type Matrix_Content is
