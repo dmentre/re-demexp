1fa2f132a36ba63bf3daf40de2a2ba4447c25d2f
David MENTRE <dmentre@linux-france.org>
2013-12-22 11:38:46 +0100

    [condorcet] Create test harness for Condorcet matrix module

---
+++ condorcet/obj/gnattest/tests/condorcet_matrix-test_data-tests.adb
@@ -0,0 +1,138 @@
+--  This package has been generated automatically by GNATtest.
+--  You are allowed to add your code to the bodies of test routines.
+--  Such changes will be kept during further regeneration of this file.
+--  All code placed outside of test routine bodies will be lost. The
+--  code intended to set up and tear down the test environment should be
+--  placed into Condorcet_Matrix.Test_Data.
+
+with AUnit.Assertions; use AUnit.Assertions;
+
+package body Condorcet_Matrix.Test_Data.Tests is
+
+
+--  begin read only
+   procedure Test_Get_Size (Gnattest_T : in out Test);
+   procedure Test_Get_Size_eeb750 (Gnattest_T : in out Test) renames Test_Get_Size;
+--  id:2.1/eeb7509557d4a2f0/Get_Size/1/0/
+   procedure Test_Get_Size (Gnattest_T : in out Test) is
+   --  condorcet_matrix.ads:10:4:Get_Size
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Get_Size;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Is_Valid_Vote (Gnattest_T : in out Test);
+   procedure Test_Is_Valid_Vote_074871 (Gnattest_T : in out Test) renames Test_Is_Valid_Vote;
+--  id:2.1/0748715fd62a6909/Is_Valid_Vote/1/0/
+   procedure Test_Is_Valid_Vote (Gnattest_T : in out Test) is
+   --  condorcet_matrix.ads:12:4:Is_Valid_Vote
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Is_Valid_Vote;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Is_Valid_Vote_Matrix (Gnattest_T : in out Test);
+   procedure Test_Is_Valid_Vote_Matrix_b82a7d (Gnattest_T : in out Test) renames Test_Is_Valid_Vote_Matrix;
+--  id:2.1/b82a7d1afd351b34/Is_Valid_Vote_Matrix/1/0/
+   procedure Test_Is_Valid_Vote_Matrix (Gnattest_T : in out Test) is
+   --  condorcet_matrix.ads:27:4:Is_Valid_Vote_Matrix
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Is_Valid_Vote_Matrix;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Is_Zero (Gnattest_T : in out Test);
+   procedure Test_Is_Zero_78dcb3 (Gnattest_T : in out Test) renames Test_Is_Zero;
+--  id:2.1/78dcb388226d19ce/Is_Zero/1/0/
+   procedure Test_Is_Zero (Gnattest_T : in out Test) is
+   --  condorcet_matrix.ads:31:4:Is_Zero
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Is_Zero;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Reset (Gnattest_T : in out Test);
+   procedure Test_Reset_b2e6b9 (Gnattest_T : in out Test) renames Test_Reset;
+--  id:2.1/b2e6b9a9fab2271c/Reset/1/0/
+   procedure Test_Reset (Gnattest_T : in out Test) is
+   --  condorcet_matrix.ads:33:4:Reset
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Reset;
+--  end read only
+
+
+--  begin read only
+   procedure Test_Vote (Gnattest_T : in out Test);
+   procedure Test_Vote_34398e (Gnattest_T : in out Test) renames Test_Vote;
+--  id:2.1/34398e546330d6d1/Vote/1/0/
+   procedure Test_Vote (Gnattest_T : in out Test) is
+   --  condorcet_matrix.ads:36:4:Vote
+--  end read only
+
+      pragma Unreferenced (Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value,
+         "Test not implemented.");
+
+--  begin read only
+   end Test_Vote;
+--  end read only
+
+end Condorcet_Matrix.Test_Data.Tests;
---
+++ condorcet/obj/gnattest/tests/condorcet_matrix-test_data-tests.ads
@@ -0,0 +1,31 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Condorcet_Matrix.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Condorcet_Matrix.Test_Data.Test
+   with null record;
+
+   procedure Test_Get_Size_eeb750 (Gnattest_T : in out Test);
+   --  condorcet_matrix.ads:10:4:Get_Size
+
+   procedure Test_Is_Valid_Vote_074871 (Gnattest_T : in out Test);
+   --  condorcet_matrix.ads:12:4:Is_Valid_Vote
+
+   procedure Test_Is_Valid_Vote_Matrix_b82a7d (Gnattest_T : in out Test);
+   --  condorcet_matrix.ads:27:4:Is_Valid_Vote_Matrix
+
+   procedure Test_Is_Zero_78dcb3 (Gnattest_T : in out Test);
+   --  condorcet_matrix.ads:31:4:Is_Zero
+
+   procedure Test_Reset_b2e6b9 (Gnattest_T : in out Test);
+   --  condorcet_matrix.ads:33:4:Reset
+
+   procedure Test_Vote_34398e (Gnattest_T : in out Test);
+   --  condorcet_matrix.ads:36:4:Vote
+
+end Condorcet_Matrix.Test_Data.Tests;
+--  end read only
